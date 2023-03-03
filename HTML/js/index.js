b06a1b49cf13d0037c06c77f71346a6033834322
JGwanghou <jogwangho0619@gmail.com>
2023-03-03 19:28:21 +0900

    0303

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -1,6 +1,6 @@
 $(function () {
   let modalPost = $(".modal-post");
-
+  let PostMenu = $(".post-menu");
   /**
    * 댓글 더보기 누를 경우 Post-Modal 뜸.
    */
@@ -13,4 +13,15 @@ $(function () {
     modalPost.css("display", "none");
     $("body").css("overflow-y", "visible");
   });
+
+  $('.sprite_more_icon').on("click", function(){
+    PostMenu.css("display", " flex");
+    $("body").css("overflow-y", "hidden");
+  })
+
+  $('.postmenu-cancel').on("click", function(){
+    PostMenu.css("display", "none");
+    $("body").css("overflow-y", "visible");
+  })
+  
 });
