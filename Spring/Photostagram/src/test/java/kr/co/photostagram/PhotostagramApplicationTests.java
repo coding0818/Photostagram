76b69303d19823a40095bec3c5fb713099e956e7
parkgayoung96 <lunatic2563@gmail.com>
2023-03-06 14:44:56 +0900

    2023/03/06

---
+++ Spring/Photostagram/src/test/java/kr/co/photostagram/PhotostagramApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.photostagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotostagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
