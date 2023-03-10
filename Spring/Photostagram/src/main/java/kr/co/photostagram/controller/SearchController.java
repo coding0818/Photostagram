68f0d8251a12148ba4e0a59d20490fedfe3c0d73
parkgayoung96 <lunatic2563@gmail.com>
2023-03-10 19:36:56 +0900

    2023/03/10 searchPage

--- Spring/Photostagram/src/main/java/kr/co/photostagram/controller/SearchController.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/controller/SearchController.java
@@ -3,28 +3,48 @@ package kr.co.photostagram.controller;
 import kr.co.photostagram.service.ProfileService;
 import kr.co.photostagram.service.SearchService;
 import kr.co.photostagram.vo.MemberVO;
+import kr.co.photostagram.vo.PostVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.List;
 
+@Slf4j
 @Controller
 public class SearchController {
 
     @Autowired
     private SearchService service;
+
     @Autowired
     private ProfileService profileService;
 
     @GetMapping(value = {"search", "search/index"})
-    public String index(Principal principal, Model model){
+    public String index(Principal principal, Model model, int no){
 
         MemberVO user =  profileService.selectMember(principal.getName());
 
-        model.addAttribute("user", user);
+        log.info("user : "+user);
+        
+        // 해시태그 관련 게시물 조회
+        List<PostVO> posts = service.selectPostGroupbyHashTag(no);
 
+        // 해시태그 이름 조회
+        String hashtagName = service.selectHashTagName(no);
+
+        // 해시태그 게시물 개수 조회
+        int countHashTag = service.selectCountHashTag(no);
+
+        log.info("posts_hashtag : "+posts);
+
+        model.addAttribute("user", user);
+        model.addAttribute("posts", posts);
+        model.addAttribute("countHashTag", countHashTag);
+        model.addAttribute("hashtagName", hashtagName);
         return "search/index";
     }
 }
--- Spring/Photostagram/src/main/java/kr/co/photostagram/service/SearchService.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/service/SearchService.java
@@ -1,9 +1,14 @@
 package kr.co.photostagram.service;
 
+import kr.co.photostagram.dao.MainDAO;
 import kr.co.photostagram.dao.SearchDAO;
-import kr.co.photostagram.vo.MemberVO;
+import kr.co.photostagram.vo.PostVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SearchService {
@@ -11,4 +16,25 @@ public class SearchService {
     @Autowired
     private SearchDAO dao;
 
+    @Autowired
+    private MainDAO mainDAO;
+
+    @Transactional
+    public List<PostVO> selectPostGroupbyHashTag(int hashtag_no){
+        int postnums[] = dao.selectPostNums(hashtag_no);
+        List<PostVO> posts = new ArrayList<>();
+        for(int num : postnums){
+            PostVO vo = dao.selectPostsByHashTag(num);
+            posts.add(vo);
+        }
+        return posts;
+    }
+
+    public int selectCountHashTag(int no){
+        return mainDAO.selectCountHashTag(no);
+    }
+
+    public String selectHashTagName(int no){
+        return dao.selectHashTagName(no);
+    }
 }
