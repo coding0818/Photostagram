16e2ea36b9e8a67b5ae1843827bd4278eb6c6202
JGwanghou <jogwangho0619@gmail.com>
2023-03-12 22:07:59 +0900

    0312

--- Spring/Photostagram/src/main/java/kr/co/photostagram/service/IndexService.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/service/IndexService.java
@@ -2,6 +2,7 @@ package kr.co.photostagram.service;
 
 import kr.co.photostagram.dao.IndexDAO;
 import kr.co.photostagram.vo.CommentVO;
+import kr.co.photostagram.vo.Comment_likeVO;
 import kr.co.photostagram.vo.PostVO;
 import kr.co.photostagram.vo.Post_likeVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,19 @@ public class IndexService {
 
     public int insertArticleLikeAdd(PostVO vo) { return dao.insertArticleLikeAdd(vo); }
     public int deleteArticleLike(PostVO vo) { return dao.deleteArticleLike(vo); }
+
+    public int insertCommentLikeAdd(Comment_likeVO vo){
+        return dao.insertCommentLikeAdd(vo);
+    };
+
+    public int deleteCommentLike(Comment_likeVO vo){
+        return dao.deleteCommentLike(vo);
+    }
+
     public int insertComment(CommentVO vo){
         return dao.insertComment(vo);
     }
 
-    public List<Post_likeVO> selectLikeConfirm() {return dao.selectLikeConfirm(); }
     public List<PostVO> selectArticles(){
         return dao.selectArticles();
     }
