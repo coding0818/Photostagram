959c49a09b3a4262712b08d8432b5300388410bc
leewang0207 <iwanggeun009@gmail.com>
2023-03-14 19:19:11 +0900

    2023/03/14

--- Spring/Photostagram/src/main/java/kr/co/photostagram/controller/BoardController.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/controller/BoardController.java
@@ -1,27 +1,49 @@
 package kr.co.photostagram.controller;
 
+import kr.co.photostagram.service.BoardService;
 import kr.co.photostagram.service.ProfileService;
+import kr.co.photostagram.vo.ImageVO;
 import kr.co.photostagram.vo.MemberVO;
+import kr.co.photostagram.vo.PostVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BoardController {
 
     @Autowired
-    private ProfileService service;
-
+    private BoardService service;
     @GetMapping("board/post")
-    public String post(Principal principal, Model model) {
-        String username = principal.getName();
-        MemberVO user = service.selectMember(username);
+    public String post(Principal principal, Model model,String username) {
+        String myName = principal.getName();
+        MemberVO member =  service.selectMember(username);
+        MemberVO user = service.selectMember(myName);
 
+        int myNo = user.getNo();          // 현재 로그인 된 사용자 번호
+        int pageNo = member.getNo();      // 프로필 페이지 사용자 번호
+
+        /*** 사용자 게시물 ***/
+        List<PostVO> posts = service.selectPosts(pageNo);           // 게시물 목록
+        Map<Integer, String> map = new HashMap<>();                 // 맵 생성
+
+        for (int i=0; i<posts.size(); i++){                         // 게시물 갯수만큼 반복
+            int postNo = posts.get(i).getNo();                      // 게시물 번호
+            String thumb = service.selectThumb(postNo).getThumb();  // 게시물 당 첫번째 사진 불러오기 (`image` 내에서 같은 `post_no` 중 가장 작은 `no`값의 `thumb`)
+            map.put(postNo, thumb);                                 // 게시물 번호(key) + 게시물 썸네일 (value)로 맵에 전달
+        }
+
+
+        model.addAttribute("member", member);
         model.addAttribute("user", user);
         return "board/post";
     }
+
 
 }
---
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/service/BoardService.java
@@ -0,0 +1,28 @@
+package kr.co.photostagram.service;
+
+import kr.co.photostagram.dao.BoardDAO;
+import kr.co.photostagram.dao.ProfileDAO;
+import kr.co.photostagram.vo.ImageVO;
+import kr.co.photostagram.vo.MemberVO;
+import kr.co.photostagram.vo.PostVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class BoardService {
+
+    @Autowired
+    private BoardDAO dao;
+
+    public ImageVO selectImg(int postNo) {return dao.selectImg(postNo);}
+    public MemberVO selectMember(String username){return dao.selectMember(username);}
+    public List<PostVO> selectPosts(int no) {return dao.selectPosts(no);}
+    public ImageVO selectThumb(int postNo) {return dao.selectThumb(postNo);}
+}
