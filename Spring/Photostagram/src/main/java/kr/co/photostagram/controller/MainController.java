c45b455e6a5cfcc8dc24aa7befd9603bbd06086c
parkgayoung96 <lunatic2563@gmail.com>
2023-03-15 19:14:24 +0900

    2023/03/15

--- Spring/Photostagram/src/main/java/kr/co/photostagram/controller/MainController.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/controller/MainController.java
@@ -9,12 +9,9 @@ import kr.co.photostagram.vo.SearchListVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,15 +25,6 @@ public class MainController {
 
     @Autowired
     private ProfileService profileService;
-
-    @GetMapping("main")
-    public String main(Principal principal, Model model){
-        MemberVO user =  profileService.selectMember(principal.getName());
-        log.info("user_no : "+user.getNo());
-
-        model.addAttribute("user", user);
-        return "main";
-    }
 
     @ResponseBody
     @PostMapping("postUpload")
