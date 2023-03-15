d58640008bab327d702fe0f0346fc21f5fd63a0c
leewang0207 <iwanggeun009@gmail.com>
2023-03-15 16:31:04 +0900

    00

--- Spring/Photostagram/src/main/java/kr/co/photostagram/dao/BoardDAO.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/dao/BoardDAO.java
@@ -9,11 +9,6 @@ import java.util.List;
 @Mapper
 @Repository
 public interface BoardDAO {
-
-   //클릭한 게시물 사진 불러오기
-   public ImageVO selectImg(int postNo);
    public MemberVO selectMember(String username);
-   public List<PostVO> selectPosts(int no);
-   public ImageVO selectThumb(int postNo);
 
 }
