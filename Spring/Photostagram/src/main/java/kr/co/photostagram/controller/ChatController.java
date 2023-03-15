8b416355a2994db6c832388354c83c604191cb5c
parkgayoung96 <lunatic2563@gmail.com>
2023-03-15 16:30:56 +0900

    2023/03/15 notice 완료

--- Spring/Photostagram/src/main/java/kr/co/photostagram/controller/ChatController.java
+++ Spring/Photostagram/src/main/java/kr/co/photostagram/controller/ChatController.java
@@ -1,13 +1,68 @@
 package kr.co.photostagram.controller;
 
+import kr.co.photostagram.service.MainService;
+import kr.co.photostagram.service.ProfileService;
+import kr.co.photostagram.vo.MemberVO;
+import kr.co.photostagram.vo.NoticeVO;
+import kr.co.photostagram.vo.SearchListVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+import java.util.List;
+
+@Slf4j
 @Controller
 public class ChatController {
 
+    @Autowired
+    private ProfileService profileService;
+
+    @Autowired
+    private MainService mainService;
+
     @GetMapping(value={"chat", "chat/index"})
-    public String chat(){
-        return "chat/index";
+    public String chat(Principal principal, Model model){
+        MemberVO user =  profileService.selectMember(principal.getName());
+        // 검색기록 요청
+        List<SearchListVO> searchList = mainService.selectSearchItemRecent(user.getNo());
+
+        log.info("user_no : "+user.getNo());
+        log.info("searchList : "+searchList);
+
+        model.addAttribute("user", user);
+        model.addAttribute("searchList", searchList);
+
+        // 알림
+        List<NoticeVO> notices = mainService.selectNotices(user.getNo());
+
+        log.info("notices : "+notices);
+
+        model.addAttribute("notices", notices);
+        return "chat/main";
+    }
+
+    @GetMapping("chat/content")
+    public String content(Principal principal, Model model){
+        MemberVO user =  profileService.selectMember(principal.getName());
+        // 검색기록 요청
+        List<SearchListVO> searchList = mainService.selectSearchItemRecent(user.getNo());
+
+        log.info("user_no : "+user.getNo());
+        log.info("searchList : "+searchList);
+
+        model.addAttribute("user", user);
+        model.addAttribute("searchList", searchList);
+
+        // 알림
+        List<NoticeVO> notices = mainService.selectNotices(user.getNo());
+
+        log.info("notices : "+notices);
+
+        model.addAttribute("notices", notices);
+        return "chat/content";
     }
 }
